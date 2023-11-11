29e45b2045c3d5bb662ef9f2977d185a3891bc00
daria <dforst09@gmail.com>
2023-11-11 20:45:31 +0300

    start js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,19 @@
 'use strict';
 
 document.addEventListener('DOMContentLoaded', () => {
+    let cards = document.querySelectorAll('.card');
+    cards.forEach(card => {
+        card.addEventListener('click', () => {
+            const cardsActive = function () {
+                cards.forEach(card => {
+                    if (card.classList.contains('active')) {
+                        return true;
+                    }
+                    else return false;
+                });
+            };
 
+            cardsActive();
+        })
+    })
 });
